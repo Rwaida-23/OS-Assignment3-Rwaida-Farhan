7785cc2493cc2886440d646de925050691226fea
Rwaida-23 <445052062@std.psau.edu.sa>
2026-05-02 22:19:04 +0300

    Add semaphore to control cpu access in process execution

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -121,7 +121,12 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
-        
+        // Acquire CPU semaphore before execution
+    try {
+    SharedResources.cpuSemaphore.acquire();
+    } catch (InterruptedException e) {
+    e.printStackTrace();
+}
         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
@@ -184,6 +189,8 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            // Release CPU semaphore after execution
+        SharedResources.cpuSemaphore.release();
         }
     }
     
